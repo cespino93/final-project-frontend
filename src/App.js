9dbbe76ac642ae88631eb7b17370e63c82b5dbc9
cespino93 <chrisl.espino@gmail.com>
2022-03-15 21:22:16 -0400

    added logged out logged in

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
 import React from 'react';
 import './App.css';
-import Login from "./components/Login.js"
-import Logout from "./components/Logout.js"
 import { connect } from 'react-redux'
 import { getCurrentUser } from "./actions/currentUser.js"
+import NavBar from './components/NavBar.js'
+import MainContainer from './components/MainContainer';
 
 class App extends React.Component {
-  
   componentDidMount() {
     this.props.getCurrentUser()
   }
 
   render() {
     return (
-      // think about refactoring this to a nav bar
-      this.props.currentUser ? <Logout/> : <Login/>
+      <div className="App">
+        <NavBar/>
+        <MainContainer/>
+      </div>
     );
   }
 }
 
-// I can do this because i know the incoming argument is an incoming object, state, coming from redux 
-// and i know it has a propertycalled currentUser
-// state = { ...,
-          // currentUser: (...)
-          // }
-const mapStateToProps = ({ currentUser }) => {
-  // return has a key of current User
-  return {
-    currentUser
-  }
-}
-
-export default connect(mapStateToProps, { getCurrentUser })(App);
+export default connect(null, { getCurrentUser })(App);
---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import Login from "./Login.js"
+import Logout from "./Logout.js"
+
+const NavBar = ({ currentUser }) => {
+    return (
+        <div className="NavBar">
+            { currentUser ? <strong>Welcome, {currentUser.name}</strong> : "" }
+            { currentUser ? <Logout/> : <Login/> }
+        </div>
+    )
+}
+
+// I can do this because i know the incoming argument is an incoming object, state, coming from redux 
+// and i know it has a propertycalled currentUser
+// state = { ...,
+          // currentUser: (...)
+          // }
+const mapStateToProps = ({ currentUser }) => {
+// return has a key of current User
+    return {
+        currentUser
+    }
+}
+           
+
+export default connect(mapStateToProps)(NavBar)
+// using { logout } as a dispatch to props object
+
+
