b839546618c50b3181b7b0026f65418e432c629f
cespino93 <chrisl.espino@gmail.com>
2022-03-17 21:30:10 -0400

    added EventCard

---
+++ src/components/EventCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Eventcard = ({ event }) => {
+    return (
+        event ?
+        <div>
+            <h3>{event.attributes.name}</h3>
+            <p>{event.attributes.start_date}</p>
+            <p>{event.attributes.end_date}</p>
+            <Link to={'/events/${event.id}/edit'}>Edit this event</Link>
+            </div> :
+            <p>This is the Event card with no event!</p>
+        </div>
+    )
+}
+
+export default EventCard
